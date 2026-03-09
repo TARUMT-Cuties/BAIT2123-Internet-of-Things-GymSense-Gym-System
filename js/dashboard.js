5fac89d0d3d1f664fdbcf1f230628f402f1c0eef
Yuki Lai <yukiilai0212@gmail.com>
2026-03-09 10:20:55 +0800

    First version

---
+++ js/dashboard.js
@@ -0,0 +1,9 @@
+const data = {
+reps:32,
+calories:180,
+exercise:"Squats"
+};
+
+document.getElementById("reps").textContent = data.reps;
+document.getElementById("calories").textContent = data.calories;
+document.getElementById("exercise").textContent = data.exercise;
